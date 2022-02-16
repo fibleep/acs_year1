b1eea26ca508233215d235a8cf550136af1d4676
fibleep <filipn924@gmail.com>
2022-02-16 16:51:47 +0100

    formatting changes

--- DATA&AI/student_questionnaire/main.py
+++ DATA&AI/student_questionnaire/main.py
@@ -1,21 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-data=pd.read_csv('Questionnaire 21-22.csv',delimiter=';',decimal='.')
+
+data = pd.read_csv('Questionnaire 21-22.csv', delimiter=';', decimal='.')
 print(data)
 data.info()
 # q1a
-blood=data[data['Blood Type']=='Unknown']
-print(len(blood)/len(data))
+blood = data[data['Blood Type'] == 'Unknown']
+print(len(blood) / len(data))
 # q1b
-license=data[data['Driver License']=='Yes']
-print(100*(len(license)/len(data)))
+license = data[data['Driver License'] == 'Yes']
+print(100 * (len(license) / len(data)))
 # q2a
-print(sum(data[data['Writing Hand']=='Left']['Mobile Devices'])/len(data[data['Writing Hand']=='Left']))
+print(sum(data[data['Writing Hand'] == 'Left']['Mobile Devices']) / len(data[data['Writing Hand'] == 'Left']))
 # q2b
-print(sum(data[data['Writing Hand']=='Right']['Mobile Devices'])/len(data[data['Writing Hand']=='Right']))
+print(sum(data[data['Writing Hand'] == 'Right']['Mobile Devices']) / len(data[data['Writing Hand'] == 'Right']))
 # q3a
-data['Shoe Size']=pd.to_numeric(data['Shoe Size'])
-data['Size']=data['Length']/data['Shoe Size']
+data['Shoe Size'] = pd.to_numeric(data['Shoe Size'])
+data['Size'] = data['Length'] / data['Shoe Size']
 print(data['Size'])
 # q3b
 print(f"max : {data['Size'].max()}")
@@ -24,5 +25,5 @@ print(f"mean: {data['Size'].mean()}")
 # q4
 print(f" sum of siblings: {sum(data['Siblings'])}")
 # q5
-data.plot.scatter(x="Travel Distance",y="Travel Time",title="Distance to KDG")
+data.plot.scatter(x="Travel Distance", y="Travel Time", title="Distance to KDG")
 plt.show()

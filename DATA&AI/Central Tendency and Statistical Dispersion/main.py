2aab686d63502031ee66c60010da7b3e6547a98e
fibleep <filipn924@gmail.com>
2022-03-16 17:30:22 +0100

    formatting changes

--- DATA&AI/Central Tendency and Statistical Dispersion/main.py
+++ DATA&AI/Central Tendency and Statistical Dispersion/main.py
@@ -1,6 +1,23 @@
 import pandas as pd
-
+import math
+#SLIDES
 laptops = pd.read_csv('laptops.csv', delimiter=';', decimal=',')
 cpuGen = ['Sandy Bridge','Ivy Bridge','Haswell','Broadwell','Skylake','Kabylake']
 laptops['cpuGeneration']=laptops['cpuGeneration'].astype(pd.CategoricalDtype(categories=cpuGen,ordered=True))
 print(laptops)
+print("------------------------")
+print(laptops.cpuGeneration.mode())
+print("------------------------")
+print(laptops.RAM.median())
+print("------------------------")
+#print(laptops.cpuGeneration.median())
+print("------------------------")
+def median_categorical(data):
+    d = data.dropna()
+    n = len(d)
+    middle=round(n/2)
+    return d.sort_values().reset_index(drop=True)[middle]
+print(median_categorical(laptops.cpuGeneration))
+print("------------------------")
+print(laptops.diskspace.mean())
+print("------------------------")
--- DATA&AI/student_questionnaire/main.py
+++ DATA&AI/student_questionnaire/main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import math
+from scipy import stats
 data = pd.read_csv('Questionnaire 21-22.csv', delimiter=';', decimal='.')
 print(data)
 data.info()
@@ -27,3 +28,31 @@ print(f" sum of siblings: {sum(data['Siblings'])}")
 # q5
 data.plot.scatter(x="Travel Distance", y="Travel Time", title="Distance to KDG")
 plt.show()
+
+# MEANS AND DEVIATIONS
+# q1a
+print("shoe size avg: ", data['Shoe Size'].mode())
+
+# q1b
+data['Shoe Size'].dropna().mean()
+# q2
+def get_outliers(data):
+    Q1 = data.quantile(0.25)
+    Q3 = data.quantile(0.75)
+    I = Q3 - Q1
+    low = Q1 - 1.5 * I
+    high = Q3 + 1.5 * I
+    return[data[~data.between(low,high)]]
+def get_extreme_outliers(data):
+    Q1 = data.quantile(0.25)
+    Q3 = data.quantile(0.75)
+    I = Q3 - Q1
+    low = Q1 - 3 * I
+    high = Q3 + 3 * I
+    return[data[~data.between(low,high)]]
+print(get_outliers(data['Shoe Size']))
+print(get_extreme_outliers(data['Shoe Size']))
+
+#q3
+print((44-data['Shoe Size'].mean())/data['Shoe Size'].std())
+print((175-data['Length'].mean())/data['Length'].std())

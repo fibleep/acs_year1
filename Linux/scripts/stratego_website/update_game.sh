fd32b6b3fdf22578a61fb2f71ca1b2d815be9c05
Filip <filip.nowak@student.kdg.be>
2022-05-05 00:08:18 +0200

    Update update_game.sh

--- Linux/scripts/stratego_website/update_game.sh
+++ Linux/scripts/stratego_website/update_game.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 IFS='
 '
-if [ ! -e  /media/game ]
-then
-mkdir /media/game
-fi
-latest = $(ls /media/game -lt | head -n2 | tail -n1)
+user='202122t13-ssh'
 
-toDelete = $(ls /media/game | wc -l)
-if [! -e /var/log ]
+if [ ! -e  /home/$user/upload ] 
+then
+mkdir /home/$user/upload
+fi
+
+if [ ! -e /var/log ]
 then
 mkdir -p /var/log
 fi
-if [ toDelete -gt 3 ]
-then
-lof = $(ls /media/game -t | tail -n+4) # keeps last 3 versions
-for file in $lof
- rm /media/game/$file
+
+latest=$(ls /home/$user/upload -lt | head -n2 | tail -n1)
+toDelete=$(ls /home/$user/upload | wc -l)
+filename=$(ls /home/$user/upload -lt | head -n2 | tail -n1 | cut -d' ' -f10)
+webPurge=$(ls /web)
+
+# CLEANING PREVIOUS FOLDER
+rm -rf stratego_web &>>/dev/null
+
+for file in $webPurge
 do
+if [[ "$file" != "legacy" ]]
+then
+rm -rf /web/$file
+fi
 done
-else
-    file = $(ls /media/game -lt | head -n2 | tail -n1 | cut -d" "-f9)
-    echo -e "LATEST FILE: $file\nDATE OF UPDATE: $(date)">>var/log/scriptlog.txt
-    echo -e "LATEST FILE: $file\n md5: $(md5sum $file)\n sha512:$(sha512sum $file)\n sha256:$(sha256sum $file)" >> /var/log/hash_codes.txt
+
+# CLONING AND UNPACKING THE WEBSITE
+git clone git@gitlab.com:fibleep/stratego_web
+mv stratego_web/* /web
+
+# DELETING OLD GAME VERSIONS
+if [ $toDelete -gt 1 ]
+then
+lof=$(ls /home/$user/upload  -t | tail -n+2)
+for file in $lof
+do
+rm /home/$user/upload/$file
+done
+# LOGGING THE SCRIPT
+    echo -e "LATEST FILE: $filename\nDATE OF UPDATE: $(date)">/home/$user/var/log/scriptlog.txt
+    echo -e "LAST UPDATE: $(date)\nLATEST FILE: $filename\n md5: $(md5sum /home/$user/upload/$filename| cut -d' ' -f1)\n sha512:$(sha512sum /home/$user/upload/$filename | cut -d' ' -f1)\n sha256:$(sha256sum /home/$user/upload/$filename | cut -d' ' -f1)" > /home/$user/var/log/hashcodes.txt
 fi

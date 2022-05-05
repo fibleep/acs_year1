d1860802561efab05d9b726aabe3999465d096c4
Filip <filip.nowak@student.kdg.be>
2022-05-05 12:44:42 +0200

    Update update_game.sh

--- Linux/scripts/stratego_website/update_game.sh
+++ Linux/scripts/stratego_website/update_game.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-IFS='
-'
 user='202122t13-ssh'
 
 if [ ! -e  /home/$user/upload ]
@@ -15,8 +13,23 @@ fi
 
 latest=$(ls /home/$user/upload -lt | head -n2 | tail -n1)
 toDelete=$(ls /home/$user/upload | wc -l)
-filename=$(ls /home/$user/upload -lt | head -n2 | tail -n1 | cut -d' ' -f10)
+filename=$(ls /home/$user/upload -t | head -1)
 webPurge=$(ls /web)
+# DELETING OLD GAME VERSIONS
+
+if [ $toDelete -gt 1 ]
+then
+lof=$(ls /home/$user/upload  -t | tail -n+2)
+for file in $lof
+do
+rm -rf /home/$user/upload/$file
+done
+# LOGGING THE SCRIPT
+    echo -e "LATEST FILE: $filename\nDATE OF UPDATE: $(date)">/home/$user/var/log/scriptlog.txt
+    echo -e "LAST UPDATE: $(date)\nLATEST FILE: $filename\n$(md5sum /home/$user/upload/$filename)\n $(sha512sum /home/$user/upload/$filename)\n$(sha256sum
+/home/$user/upload/$filename)"
+fi
+
 
 # CLEANING PREVIOUS FOLDER
 
@@ -27,20 +40,15 @@ then
 rm -rf /web/$file
 fi
 done
+rm -rf /web/$filename &>/dev/null
+cp /home/$user/upload/$filename /web/$filename
+cp /home/$user/var/log/hashcodes.txt /web/hashcodes.txt
 
 # CLONING AND UNPACKING THE WEBSITE
-pushd /web/legacy/stratego_web;git pull git@gitlab.com:fibleep/stratego_web;popd
-mv /web/legacy/stratego_web/* /web
+pushd /web/legacy/stratego_web;git pull --rebase git@gitlab.com:fibleep/stratego_web;popd
+cp -r /web/legacy/stratego_web/* /web
 
-# DELETING OLD GAME VERSIONS
-if [ $toDelete -gt 1 ]
+if [ $? ]
 then
-lof=$(ls /home/$user/upload  -t | tail -n+2)
-for file in $lof
-do
-rm /home/$user/upload/$file
-done
-# LOGGING THE SCRIPT
-    echo -e "LATEST FILE: $filename\nDATE OF UPDATE: $(date)">/home/$user/var/log/scriptlog.txt
-    echo -e "LAST UPDATE: $(date)\nLATEST FILE: $filename\n md5: $(md5sum /home/$user/upload/$filename| cut -d' ' -f1)\n sha512:$(sha512sum /home/$user/upload/$filename | cut -d' ' -f1)\n sha256:$(sha256sum /home/$user/upload/$filename | cut -d' ' -f1)" > /home/$user/var/log/hashcodes.txt
+echo "\n git pull successful!" >> /home/$user/var/log/scriptlog.txt
 fi
